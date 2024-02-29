ef072adefaa1aedbcfff04409e2b42e11dfbb7f2
david <davidsanchezdiaz72@gmail.com>
2024-02-29 18:51:23 +0100

    Crear mensajes de éxito y errores en la vista Blade

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -6,6 +6,20 @@
     <form action="{{ route('todos') }}" method="POST"> <!-- Formulario que enviará los datos a la ruta 'todos' mediante el método POST -->
         @csrf <!-- Directiva de Blade para incluir el token CSRF -->
 
+        @if (session('success')) <!-- Verifica si hay un mensaje de éxito en la sesión -->
+
+            <h6 class="alert alert-success">{{ session('success') }}</h6> <!-- Muestra un mensaje de éxito utilizando la clase de alerta de Bootstrap -->
+    
+        @endif <!-- Fin de la verificación de éxito -->
+        
+        @error('title') <!-- Verifica si hay errores en el campo 'title' -->
+        
+            <h6 class="alert alert-danger">{{ $message }}</h6> <!-- Muestra el mensaje de error asociado al campo 'title' -->
+        
+        @enderror <!-- Fin de la verificación de errores en el campo 'title' -->
+    
+    
+
         <div class="mb-3">
             <label for="title" class="form-label">Título de la tarea</label> <!-- Etiqueta para el campo de entrada de texto -->
             <input type="text" name="title" class="form-control" > <!-- Campo de entrada de texto para el título de la tarea -->
